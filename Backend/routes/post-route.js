cd0fc2c94bbf9653307de34e94ee892ad8a94dce
Akashjadhav55 <akashjadhav7040253178@gmail.com>
2023-02-27 21:16:22 +0530

    done

--- Backend/routes/post-route.js
+++ Backend/routes/post-route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { addPost, deletePost, getAllPost, getById, getByUserId, updatePost } from "../Controllers/post-controller";
 const blogrouter = express.Router()
 
-// blogrouter.get("/", getAllPost)
+blogrouter.get("/", getAllPost)
 blogrouter.post("/add", addPost)
 blogrouter.patch("/update/:id", updatePost)
 blogrouter.get("/:id", getById)
